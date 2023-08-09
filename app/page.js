a7a15b3c5457faf80e6ed148400b0c630368d1e9
Tysmithyyy <tylerelijahsmith15@gmail.com>
2023-08-09 13:11:49 -0600

    updated to make sure bot reads chat history, added avatars for chef and user, updated favicon

--- app/page.js
+++ app/page.js
@@ -6,8 +6,11 @@ import LoadingAnimation from "./components/LoadingAnimation";
 
 export default function Home() {
   const [inputValue, setInputValue] = useState('')
-  const [chatLog, setChatLog] = useState([])
+  const [chatLog, setChatLog] = useState([
+    { role: 'assistant', content: "Hi! I'm Chef Bot. Feel free to ask me any cooking questions, or just let me know if you want to discuss something cooking related." }
+  ])
   const [isLoading, setIsLoading] = useState(false)
+
 
   useEffect(() => {
     // Fetch data here (if needed) or perform other side effects
@@ -15,15 +18,12 @@ export default function Home() {
     // You can also return a cleanup function if needed
     // For example, if you set up subscriptions, you can clean them up here
 
-    // return () => {
-    //   // Cleanup logic here (if needed)
-    // }
   }, []);
 
   const handleSend = (event) => {
     event.preventDefault();
 
-    setChatLog((prevChatLog) => [...prevChatLog, {type: 'user', message: inputValue}]);
+    setChatLog((prevChatLog) => [...prevChatLog, {role: 'user', content: inputValue}]);
 
     sendMessage(inputValue)
 
@@ -37,18 +37,27 @@ export default function Home() {
       'Content-type': 'application/json',
       'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`
     };
+
+    // Create the messageLog by concatenating the existing chat log and the new message
+    const newMessage = { role: 'user', content: message };
+    const messageLog = [...chatLog, newMessage].map((message) => ({ role: message.role, content: message.content }));
+
+    console.log(messageLog)
+
+    console.log(chatLog)
+
     const data = {
-      model:'gpt-3.5-turbo-16k',
-      messages:[{'role':'user', 'content':message}]
+      model: 'gpt-3.5-turbo',
+      messages: messageLog
     };
 
-    console.log(headers.Authorization)
+    
 
     setIsLoading(true);
 
     axios.post(url, data, {headers: headers}).then((response) => {
       console.log(response);
-      setChatLog((prevChatLog) => [...prevChatLog, {type: 'bot', message: response.data.choices[0].message.content}]);
+      setChatLog((prevChatLog) => [...prevChatLog, {role: 'assistant', content: response.data.choices[0].message.content}]);
       setIsLoading(false);
     }).catch((error) => {
       setIsLoading(false);
@@ -60,18 +69,24 @@ export default function Home() {
   return (
     <main className="bg-gray-900">
       <div className="container mx-auto flex flex-col h-screen max-w-[700px]">
-        <h1 className="bg-gradient-to-r from-blue-600 to-blue-300 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">AI Chat Bot</h1>
+        <h1 className="bg-gradient-to-r from-blue-600 to-blue-300 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">Chef Bot</h1>
         <div className="flex-grow overflow-scroll p-6">
           <div className="flex flex-col space-y-4">
           {chatLog.map((message, index) => (
-            <div key={index} className={`flex ${
-              message.type === 'user' ? 'justify-end' : 'justify-start'
-              }`}>
-              <div className={`${
-                message.type === 'user' ? 'bg-blue-500' : 'bg-gray-800'
-              } rounded-lg p-4 text-white max-w-sm`}>
-              {message.message}
+            <div key={index} className={`flex ${message.role === 'user' ? 'justify-end' : 'justify-start'} items-center`}>
+              {/* User Avatar */}
+              {message.role === 'assistant' && (
+                  <span className="text-4xl pr-3">👨‍🍳</span>
+              )}
+              <div className={`${message.role === 'user' ? 'bg-blue-500' : 'bg-gray-800'} rounded-lg p-4 text-white max-w-sm`}>
+                {message.content}
               </div>
+              {/* Assistant Avatar */}
+              {message.role === 'user' && (
+                <div className="w-8 h-8 ml-2">
+                  <span className="text-4xl pl-0.5">🐵</span>
+                </div>
+              )}
             </div>
           ))
           }
